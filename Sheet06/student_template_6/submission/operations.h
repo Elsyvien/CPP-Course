239f4e9725c8eb0f4082a25247dc4b6e6b0814b1
Max <max@staneker.com>
2025-09-25 15:28:34 +0200

    Add And, Or, Xor, and Not operation classes
    Introduced new final classes for boolean operations (And, Or, Xor, Not) inheriting from Operation. Each class provides overrides for clone_impl and isInside_impl methods to support custom shape operations.

--- Sheet06/student_template_6/submission/operations.h
+++ Sheet06/student_template_6/submission/operations.h
@@ -14,4 +14,38 @@ public:
 private:
     /// by default, operations between two shapes have a result that is contained within their combined bounds
     AABB getBounds_impl() const override;
+    //Shape clone_impl() const override;
 };
+
+class And final : public Operation {
+public:
+    And(const Shape& shape_a, const Shape& shape_b) : Operation(shape_a, shape_b) {}
+private: 
+    Shape clone_impl() const override;
+    bool isInside_impl(const Point3D& p) const override;
+};
+
+class Or final : public Operation {
+public: 
+    Or(const Shape& shape_a, const Shape& shape_b) : Operation(shape_a, shape_b) {}
+private:
+    Shape clone_impl() const override;
+    bool isInside_impl(const Point3D& p) const override;
+};
+
+class Xor final : public Operation {
+public: 
+    Xor(const Shape& shape_a, const Shape& shape_b) : Operation(shape_a, shape_b) {}
+private:
+    Shape clone_impl() const override;
+    bool isInside_impl(const Point3D& p) const override;
+};
+
+class Not final : public Operation{
+public:
+    Not(const Shape& shape_a, const Shape& shape_b) : Operation(shape_a, shape_b) {}
+private:
+    Shape clone_impl() const override;
+    bool isInside_impl(const Point3D& p) const override;
+};
+
