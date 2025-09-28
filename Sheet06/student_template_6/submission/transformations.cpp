5c2945c9b2e11382774b792812bb5ce3443d20ab
Max <max@staneker.com>
2025-09-28 23:15:59 +0200

    Add axis support to Rotated and implement getBounds
    Enhanced the Rotated class to support rotation around X, Y, and Z axes in isInside_impl. Added getBounds_impl to compute the bounding box of rotated shapes by transforming all corners according to the selected axis.

--- Sheet06/student_template_6/submission/transformations.cpp
+++ Sheet06/student_template_6/submission/transformations.cpp
@@ -53,7 +53,77 @@ Shape Rotated::clone_impl() const {
 }
 
 bool Rotated::isInside_impl(const Point3D& p) const {
-    std::pair<float, float> xy = rotate2D(p.x, p.y, -angle);
-    Point3D rotated{xy.first, xy.second, p.z};
+    Point3D rotated = p;
+
+    if (axis == Axis::Z) {
+        auto xy = rotate2D(p.x, p.y, -angle);
+        rotated.x = xy.first;
+        rotated.y = xy.second;
+    } else if (axis == Axis::X) {
+        auto yz = rotate2D(p.y, p.z, -angle);
+        rotated.y = yz.first;
+        rotated.z = yz.second;
+    } else if (axis == Axis::Y) {
+        auto xz = rotate2D(p.x, p.z, angle);
+        rotated.x = xz.first;
+        rotated.z = xz.second;
+    }
+
     return sub_shape.isInside(rotated);
+}
+
+AABB Rotated::getBounds_impl() const {
+    AABB original = sub_shape.getBounds();
+
+    Point3D originalMin = original.min;
+    Point3D originalMax = original.max;
+
+    Point3D ecken[8] = {
+        {originalMin.x, originalMin.y, originalMin.z}, {originalMin.x, originalMin.y, originalMax.z},
+        {originalMin.x, originalMax.y, originalMin.z}, {originalMin.x, originalMax.y, originalMax.z},
+        {originalMax.x, originalMin.y, originalMin.z}, {originalMax.x, originalMin.y, originalMax.z},
+        {originalMax.x, originalMax.y, originalMin.z}, {originalMax.x, originalMax.y, originalMax.z}
+    };
+
+    Point3D rotateOrigin = ecken[0];
+    if (axis == Axis::Z) {
+        std::pair<float, float> xy = rotate2D(rotateOrigin.x, rotateOrigin.y, angle);
+        rotateOrigin.x = xy.first;
+        rotateOrigin.y = xy.second;
+    } else if (axis == Axis::X) {
+        std::pair<float, float> yz = rotate2D(rotateOrigin.y, rotateOrigin.z, angle);
+        rotateOrigin.y = yz.first;
+        rotateOrigin.z = yz.second;
+    } else if (axis == Axis::Y) {
+        std::pair<float, float> xz = rotate2D(rotateOrigin.x, rotateOrigin.z, -angle);
+        rotateOrigin.x = xz.first;
+        rotateOrigin.z = xz.second;
+    }
+    Point3D newMin = rotateOrigin;
+    Point3D newMax = rotateOrigin;
+    // TODO: Bessere lösung finden
+    for (int i = 1; i < 8; ++i) { // meh irgendwie unschön
+        Point3D next = ecken[i];
+        if (axis == Axis::Z) {
+            std::pair<float, float> xy = rotate2D(next.x, next.y, angle);
+            next.x = xy.first;
+            next.y = xy.second;
+        } else if (axis == Axis::X) {
+            std::pair<float, float> yz = rotate2D(next.y, next.z, angle);
+            next.y = yz.first;
+            next.z = yz.second;
+        } else if (axis == Axis::Y) {
+            std::pair<float, float> xz = rotate2D(next.x, next.z, -angle);
+            next.x = xz.first;
+            next.z = xz.second;
+        }
+        newMin.x = std::min(newMin.x, next.x);
+        newMin.y = std::min(newMin.y, next.y);
+        newMin.z = std::min(newMin.z, next.z);
+        newMax.x = std::max(newMax.x, next.x);
+        newMax.y = std::max(newMax.y, next.y);
+        newMax.z = std::max(newMax.z, next.z);
+    }
+
+    return {newMin, newMax};
 }
