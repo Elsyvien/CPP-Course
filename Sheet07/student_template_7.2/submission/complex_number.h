021af06b259263c770b633985db41ce3ba140f50
Max <max@staneker.com>
2025-09-29 15:17:40 +0200

    Implement generic math functions and ComplexNumber class
    Added template implementations for square, halve, add, multiply, reduce, and map functions in exercise_07.h. Refactored ComplexNumber to be a template class, implemented operator+=, operator+, and stream output operator for generic types in complex_number.h.

--- Sheet07/student_template_7.2/submission/complex_number.h
+++ Sheet07/student_template_7.2/submission/complex_number.h
@@ -1,40 +1,63 @@
+/*
+Philipp Schmid: 5703070
+Max Staneker: 7018590
+*/
 #pragma once
 
 #include <iostream>
 
 // TODO: task 7.2 a)
+template<typename T>
 class ComplexNumber
 {
 public:
     ComplexNumber() noexcept = default;
-    ComplexNumber(double real, double imaginary = 0.0) : real_number(real), imaginary_number(imaginary) {};
+    ComplexNumber(T real, T imaginary = T{}) : real_number(real), imaginary_number(imaginary) {};
 
     ComplexNumber& operator+=(const ComplexNumber& rhs);
 
-    friend std::ostream& operator<<(std::ostream&, const ComplexNumber&);
+    template<typename U>
+    friend std::ostream& operator<<(std::ostream&, const ComplexNumber<U>&);
 
-    double getIm() const { return imaginary_number; };
-    double getRe() const { return real_number; }
+    T getIm() const { return imaginary_number; };
+    T getRe() const { return real_number; }
 
 private:
-    double real_number{};
-    double imaginary_number{};
+    T real_number{};
+    T imaginary_number{};
 };
 
 // TODO: task 7.2 b) - unary operator+= (class member, modifying)
-ComplexNumber& ComplexNumber::operator+=(const ComplexNumber& other)
+template<typename T>
+ComplexNumber<T>& ComplexNumber<T>::operator+=(const ComplexNumber<T>& other)
 {
     // TODO
+    real_number += other.real_number;
+    imaginary_number += other.imaginary_number;
+    return *this;
 }
 
 // TODO: task 7.2 b) - binary operator+ (free function, non-modifying: returns new complex number)
-ComplexNumber operator +(const ComplexNumber& lhs, const ComplexNumber& rhs)
+template<typename T>
+ComplexNumber<T> operator +(ComplexNumber<T> lhs, const ComplexNumber<T>& rhs)
 {
     // TODO
-    return {};
+    lhs += rhs;
+    return lhs;
 }
 
-std::ostream& operator<<(std::ostream& out, const ComplexNumber& complex)
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const ComplexNumber<T>& complex)
 {
     // TODO
+    //real part
+    out << complex.getRe();
+    //imaginary part
+    T imaginary = complex.getIm();
+    if (imaginary >= T{}) {
+        out << '+' << imaginary << 'i';}
+    else {
+        out << imaginary << 'i';
+    }
+    return out;
 }
