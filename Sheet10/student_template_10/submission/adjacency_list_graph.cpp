c80c7566fadb69409110923fd276426fc0b6b8f5
Max <90206985+Elsyvien@users.noreply.github.com>
2025-09-30 22:57:05 +0000

    added sheet10 exercise

--- Sheet10/student_template_10/submission/adjacency_list_graph.cpp
+++ Sheet10/student_template_10/submission/adjacency_list_graph.cpp
@@ -1,3 +1,8 @@
+/*
+Philipp Schmid
+Max Staneker
+*/
+
 #include "adjacency_list_graph.h"
 
 #include <stdexcept>
@@ -6,24 +11,55 @@
 
 void detail::AdjacencyListGraphBase::add_edge(uint32_t from, uint32_t to, float weight)
 {
-    // TODO: task 10.1 a)
+    size_t num_nodes = get_num_nodes();
+    if (from >= num_nodes) {throw std::runtime_error("Error Start doesn't exist");}
+    if (edges.size() <= from) {
+        edges.resize(from + 1);
+    }
+
+    for (size_t i = 0; i < edges[from].size(); i++) {
+        if (edges[from][i].first == to) throw std::runtime_error("Edge alread exists");
+    }
+
+    edges[from].push_back(std::make_pair(to, weight));
 
 }
 
 void detail::AdjacencyListGraphBase::remove_edge(uint32_t from, uint32_t to)
 {
-    // TODO: task 10.1 b)
+    size_t num_nodes = get_num_nodes();
+    if (from >= num_nodes) {std::runtime_error("Error Start doesn't exist");}
+    if (edges.size() <= from || edges[from].empty()) {throw std::runtime_error("Error edge doesnt exist");}
 
+    for (size_t i = 0; i < edges[from].size();i++){
+        if (edges[from][i].first == to) {
+            edges[from].erase(edges[from].begin() + i);
+            return;
+        }
+    }
+    throw std::runtime_error("Edge doesnt exist");
 }
 
 std::optional<float> detail::AdjacencyListGraphBase::get_edge(uint32_t from, uint32_t to) const
 {
-    // TODO: task 10.1 c)
+    size_t num_nodes = get_num_nodes();
+    if (from >= num_nodes || to >= num_nodes) {throw std::range_error("OUT OF RANGE!");} 
+    if (edges.size() <= from) {return std::optional<float>();}
 
+    const auto& edgesFrom = edges[from];
+    for (auto& edge : edgesFrom) {
+        if (edge.first == to) {
+            return edge.second;
+        }
+    }
+    return std::optional<float>();
 }
 
 const std::vector<std::pair<uint32_t, float>>& detail::AdjacencyListGraphBase::get_edges_starting_at(uint32_t node) const
 {
-    // TODO: task 10.1 d)
+    const std::vector<std::pair<uint32_t, float>> empty_edges;
+    if (node >= get_num_nodes()) {return empty_edges;}
+    if (node >= edges.size()) {return empty_edges;}
 
+    return edges[node];
 }
