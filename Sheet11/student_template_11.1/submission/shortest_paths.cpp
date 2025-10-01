375f1ec29172805c72c4b183a0179ce60213735e
Max <90206985+Elsyvien@users.noreply.github.com>
2025-10-01 15:15:04 +0200

    Update shortest_paths.cpp

--- Sheet11/student_template_11.1/submission/shortest_paths.cpp
+++ Sheet11/student_template_11.1/submission/shortest_paths.cpp
@@ -20,8 +20,71 @@ std::vector<size_t> ShortestPaths::compute_shortest_path(size_t from, size_t to)
     size_t num_visited = 0;
 
     // TODO: your code here
+    const size_t n = adjacency_matrix.size();
+    if (n == 0) return result;
+    if (from < 0 || from >=n) return result;
+    if (to < 0 || to >=n) return result;
 
+    const float infinity = std::numeric_limits<float>::infinity();
+    const size_t undefined = std::numeric_limits<size_t>::max();
+
+    std::vector<float> distance(n, infinity);
+    std::vector<bool> visited(n, false);
+    std::vector<size_t> queue;
+    std::vector<size_t> previous(n, undefined);
+
+    queue.push_back(from);
+
+    for (auto i = 0; i < n; i++) { queue[i] = i; }
+
+    distance[from] = 0.0f; 
+    
+    while (num_visited < n) {
+        auto bestDistance = infinity;
+        auto bestIndex = undefined;
+
+        for (auto i = 0; i < n; i++) {
+            auto id = queue[i];
+            if (!visited[id] && distance[id] < bestDistance) {
+                bestDistance = distance[id];
+                bestIndex = id;
+            }
+        }
+
+        if (bestIndex == undefined) {break;} // no nodes
+
+        queue[bestIndex] = undefined; 
+        num_visited++;
+
+        if (bestIndex == to) { break; } // reached destination
+
+        const auto& edges = adjacency_matrix[bestIndex];
+        for (auto i = 0; i < n; i++) {
+            if (queue[i] == undefined) { continue; }
+            if (!edges[i].has_value()) { continue; }
+
+            float weight = edges[i].value();
+            float newDistance = distance[bestIndex] + weight;
+
+            if (newDistance < distance[i]) {
+                distance[i] = newDistance;
+                previous[i] = bestIndex;
+            }
+        }
+    }
+    if (distance[to] == infinity) {
+        std::cout << "Nodes visited: " << num_visited << std::endl;
+        return result; 
+    }
+
+    auto current = to;
+    while (current != undefined) {
+        result.push_back(current);
+        if (current == from) break;
+        current = previous[current];
+    }
+
+    std::reverse(result.begin(), result.end());
     std::cout << "Nodes visited: " << num_visited << std::endl;
-
     return result;
 }
